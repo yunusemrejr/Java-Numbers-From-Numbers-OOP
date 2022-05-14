64aa441e74a6dda76255796ad235a1e6978e79a0
YUNUS EMRE VURGUN <64273622+yunusemrejr@users.noreply.github.com>
2022-05-14 22:51:58 +0300

    Add files via upload

---
+++ Calculations.java
@@ -0,0 +1,93 @@
+public class Calculations {
+
+    private float number;
+    private String text;
+
+    private float timesPi;
+
+    private String timesPiText;
+
+    private float squareroot;
+
+    private String squarerootText;
+
+
+    public void setNumber(float num){
+
+     number = num;
+
+    }
+
+    public void setText(String txt){
+
+    text = txt;
+
+    }
+
+    public float getNumber(){
+
+        return number;
+
+    }
+
+    public String getText(){
+
+        return text;
+
+    }
+
+    public void setTimesPi(float timesPi2){
+
+        timesPi=timesPi2;
+
+
+    }
+
+    public void setTimesPiText(String timesPiText2){
+
+        timesPiText=timesPiText2;
+
+
+    }
+
+    public float getTimesPi(){
+
+      return timesPi;
+
+    }
+
+    public String getTimePiText(){
+
+      return timesPiText;
+
+    }
+
+
+    public void setSquareroot(float sqrt){
+
+        squareroot=sqrt;
+
+
+    }
+
+    public void setSquarerootText(String sqrtText){
+
+        squarerootText=sqrtText;
+
+
+    }
+
+    public float getSquareroot(){
+
+        return squareroot;
+
+    }
+
+    public String getSquarerootText(){
+
+        return squarerootText;
+
+    }
+
+
+}
