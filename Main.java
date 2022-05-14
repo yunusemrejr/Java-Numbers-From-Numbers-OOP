5cfe7a8c41f2a02e6c6accf9695be98289a3532d
YUNUS EMRE VURGUN <64273622+yunusemrejr@users.noreply.github.com>
2022-05-14 22:53:19 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,28 +4,43 @@ import static java.lang.Math.sqrt;
 
 public class Main {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
+
         float input;
+
         Calculations calculations = new Calculations();
+
         System.out.println("Welcome to Numbers From Numbers.");
+
         System.out.println("Simply give a number get more numbers! Type below:");
+
         Scanner scanner = new Scanner(System.in);
 
-        try{
-              input = Float.parseFloat( scanner.nextLine() );
+
+        try {
+            input = Float.parseFloat(scanner.nextLine());
+
             calculations.setNumber(input);
+
             calculations.setText("you entered: ");
-            System.out.println( calculations.getText() + calculations.getNumber() );
 
-            calculations.setTimesPi((float)(input*3.14159265359));
+            System.out.println(calculations.getText() + calculations.getNumber());
+
+            calculations.setTimesPi((float) (input * 3.14159265359));
+
             calculations.setTimesPiText("Your number * Pi :");
-            System.out.println(calculations.getTimePiText()+calculations.getTimesPi());
 
-            calculations.setSquareroot((float)(sqrt(input)));
+            System.out.println(calculations.getTimePiText() + calculations.getTimesPi());
+
+            calculations.setSquareroot((float) (sqrt(input)));
+
             calculations.setSquarerootText("Square root of your number :");
-            System.out.println(calculations.getSquarerootText()+calculations.getSquareroot());
 
-        }catch(Exception e){
+            System.out.println(calculations.getSquarerootText() + calculations.getSquareroot());
+
+
+        } catch (Exception e) {
+
             System.out.println("Wrong input format.");
         }
 
